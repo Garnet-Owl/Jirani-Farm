b13ec67144967bed6759aab39ef958e50ec805d8
james <james1997mburu@gmail.com>
2023-07-31 22:56:12 +0300

    coreected urls

--- j_farm_project/j_farm_project/urls.py
+++ j_farm_project/j_farm_project/urls.py
@@ -22,4 +22,21 @@ urlpatterns = {
 
     path('admin/', admin.site.urls),
     path('', include('Jirani.urls')),
+    path('Jirani/', include('Jirani.urls')),
+    path('Jirani/register/', include('Jirani.urls')),
+    path('Jirani/login/', include('Jirani.urls')),
+    path('Jirani/logout/', include('Jirani.urls')),
+    path('Jirani/profile/', include('Jirani.urls')),
+    path('Jirani/tractors/', include('Jirani.urls')),
+    path('Jirani/tractors/<int:tractor_id>/', include('Jirani.urls')),
+    path('Jirani/tractors/add/', include('Jirani.urls')),
+    path('Jirani/tractors/edit/<int:tractor_id>/', include('Jirani.urls')),
+    path('Jirani/tractors/delete/<int:tractor_id>/', include('Jirani.urls')),
+    path('Jirani/leases/', include('Jirani.urls')),
+    path('Jirani/leases/<int:lease_id>/', include('Jirani.urls')),
+    path('Jirani/leases/create/<int:tractor_id>/', include('Jirani.urls')),
+    path('Jirani/leases/cancel/<int:lease_id>/', include('Jirani.urls')),
+    path('Jirani/leases/confirm/<int:lease_id>/', include('Jirani.urls')),
+    path('Jirani/leases/complete/<int:lease_id>/', include('Jirani.urls')),
+
 }
